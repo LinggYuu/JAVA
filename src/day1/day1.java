e6781ff425b3d70d07023af7df2d5e53abdbf483
Linggg <lingyu4113@gmail.com>
2021-01-01 16:11:13 +0800

    01

---
+++ src/day1/day1.java
@@ -0,0 +1,19 @@
+package day1;
+
+/*
+变量的使用
+ */
+
+
+
+public class day1 {
+    public static void main(String[] args) {
+        int myAge=22;//变量声明+赋值
+        System.out.println(myAge);
+
+    }
+
+    public void method(){
+        int myClass=1;
+    }//myClass的作用域仅限于包含它的一对大括号中
+}
